aa5cde9a2c3afd9844abb779aa8325a8f7b60ded
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-08-17 18:20:35 -0400

    mergesort

--- mergeSort.js
+++ mergeSort.js
@@ -26,4 +26,19 @@ function merge(arr1, arr2) {
   return arr;
 }
 
+function mergeSort(arr) {
+  let n = arr.length;
+  let mid = Math.floor(arr.length / 2);
+
+  let arr1 = arr.slice(0, mid);
+  let arr2 = arr.slice(mid, n);
+
+  if (arr1.length <= 1 && arr2.length <= 1) {
+    return merge(arr1, arr2);
+  }
+
+  return merge(mergeSort(arr1), mergeSort(arr2));
+}
+
 console.log(merge([1, 4, 7, 9, 15, 20], [2, 3, 6, 10]));
+console.log(mergeSort([1, 9, 2, 6, 4, 3, 0]));
