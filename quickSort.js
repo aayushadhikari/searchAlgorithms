8b6eeff8bcf4945fb88a031e5cbbcfe475b24a8b
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-08-18 16:15:22 -0400

    pibvot

--- quickSort.js
+++ quickSort.js
@@ -1,32 +1,31 @@
-function pivot(arr, start = 0, end = arr.length - 1) {
-  const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-  };
-
-  // We are assuming the pivot is always the first element
+function pivot(arr, start, end) {
   let pivot = arr[start];
-  let swapIdx = start;
+  let pivotIndex = start;
 
   for (let i = start + 1; i <= end; i++) {
     if (pivot > arr[i]) {
-      swapIdx++;
-      swap(arr, swapIdx, i);
+      pivotIndex++;
+
+      let temp = arr[pivotIndex];
+      arr[pivotIndex] = arr[i];
+      arr[i] = temp;
     }
   }
-
-  // Swap the pivot from the start the swapPoint
-  swap(arr, start, swapIdx);
-  return swapIdx;
+  let temp = arr[pivotIndex];
+  arr[pivotIndex] = arr[start];
+  arr[start] = temp;
+  return pivotIndex;
 }
 
 function quickSort(arr, left = 0, right = arr.length - 1) {
   if (left < right) {
-    let pivotIndex = pivot(arr, left, right); //3
-    //left
+    let pivotIndex = pivot(arr, left, right);
+
     quickSort(arr, left, pivotIndex - 1);
-    //right
     quickSort(arr, pivotIndex + 1, right);
   }
+
+  //console.log(arr);
   return arr;
 }
 
