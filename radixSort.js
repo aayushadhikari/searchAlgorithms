687c76b477d08eda4b8ec2db634b5da50109e51c
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-08-18 16:42:11 -0400

    helper

--- radixSort.js
+++ radixSort.js
@@ -2,4 +2,17 @@ function getDigit(number, digit) {
   return Math.floor(Math.abs(number) / Math.pow(10, digit)) % 10;
 }
 
-console.log(getDigit(12345, 5));
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) {
+  let max = 0;
+  for (num of nums) {
+    max = Math.max(max, digitCount(num));
+  }
+  return max;
+}
+
+console.log(mostDigits([1, 2, 34, 567, 1235]));
